6981062365f80c85544ce74ca941a0f63e1e9157
Sokolov <sokolov@Sokolovs-MacBook-Air.local>
2023-02-02 13:16:58 +0300

    dtHw.10 Keychain fix

--- Navigation/Navigation/AppDelegate.swift
+++ Navigation/Navigation/AppDelegate.swift
@@ -23,29 +23,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let keychain = Keychain(service: "ForRealm.token")
         
-        var key = Data(count: 64)
-        key.withUnsafeMutableBytes({ (pointer: UnsafeMutableRawBufferPointer) in
-            _ = SecRandomCopyBytes(kSecRandomDefault, 64, pointer.baseAddress!) })
-        
-        keychain[data:"secret"] = key
-        
-        let secretData = keychain[data: "secret"]
-        
-        print("KEY: ", key)
-        print("SECRET DATA: ", secretData!)
-        
-        var config = Realm.Configuration(encryptionKey: secretData ) //без обертывания в кейчейн, все ок
-        
-        config.schemaVersion = 3
-        Realm.Configuration.defaultConfiguration = config
+        if keychain[data: "secret"] == nil {
+            
+            var key = Data(count: 64)
+            key.withUnsafeMutableBytes({ (pointer: UnsafeMutableRawBufferPointer) in
+                _ = SecRandomCopyBytes(kSecRandomDefault, 64, pointer.baseAddress!) })
+            
+            keychain[data:"secret"] = key
+                        
+        } else {
+            
+            let secretData = keychain[data: "secret"]
+                        
+            var config = Realm.Configuration(encryptionKey: secretData )
+            
+            config.schemaVersion = 3
+            Realm.Configuration.defaultConfiguration = config
+            
+            do {
                 
-        do {
-            
-            _ = try Realm(configuration: config)
-            
-        } catch let error as NSError {
-
-            fatalError("Error opening realm: \(error)")
+                _ = try Realm(configuration: config)
+                
+            } catch let error as NSError {
+                
+                fatalError("Error opening realm: \(error)")
+            }
         }
                 
         
